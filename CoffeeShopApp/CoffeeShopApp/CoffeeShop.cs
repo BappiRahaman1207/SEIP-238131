286fff4f89ccb5ebe76b9c5862cec3fc731a8e99
BappiRahaman1207 <coolbappi8888@gmail.com>
2019-05-22 20:44:56 +0600

    added_folder_added

---
+++ CoffeeShopApp/CoffeeShopApp/CoffeeShop.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoffeeShopApp
+{
+    public partial class CoffeeShop : Form
+
+    {
+        int numberOfQuantity = 1;
+        double blackCoffeePrice = 120;
+        double coldCoffeePrice = 120;
+        double hotCoffeePrice = 120;
+        double regularCoffeePrice = 120;
+        string customerName, contactNo, customerAddress, orderBox;
+        double totalPrice = 0;
+    
+
+    
+        public CoffeeShop()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            customerName = nameTextBox.Text;
+            contactNo = contactTextBox.Text;
+            customerAddress = addressTextBox.Text;
+            orderBox = OrderComboBox.Text;
+            numberOfQuantity = Convert.ToInt32(quantityTextBox.Text);
+
+            if (orderBox == "Black")
+
+            {
+                totalPrice = blackCoffeePrice * numberOfQuantity;
+                displayRichTextBox.Text = "\nCustomer Information\n\n" + "\nCustomer Name     : " + customerName + "\n\n"
+                + "\nContact No            : " +contactNo + "\n\n" + "\nCustomer Address  : " +customerAddress + "\n\n"
+                + "\nOrder                    : " + orderBox + "\n\n" + "\nQuantity                : " + numberOfQuantity + "\n\n\n" + "   Total Price  : "
+                + totalPrice + "\n\n";
+                
+            }
+
+            else if (orderBox == "Cold")
+
+            {
+
+                totalPrice = coldCoffeePrice * numberOfQuantity;
+                displayRichTextBox.Text = "\nCustomer Information\n\n" + "\nCustomer Name     : " + customerName + "\n\n"
+                + "\nContact No            : " + contactNo + "\n\n" + "\nCustomer Address  : " + customerAddress + "\n\n"
+                + "\nOrder                    : " + orderBox + "\n\n" + "\nQuantity                : " + numberOfQuantity + "\n\n\n" + "   Total Price  : "
+                + totalPrice + "\n\n";
+
+            }
+
+            else if (orderBox == "Hot")
+
+            {
+
+                totalPrice = hotCoffeePrice * numberOfQuantity;
+                displayRichTextBox.Text = "\nCustomer Information\n\n" + "\nCustomer Name     : " + customerName + "\n\n"
+                + "\nContact No            : " + contactNo + "\n\n" + "\nCustomer Address  : " + customerAddress + "\n\n"
+                + "\nOrder                    : " + orderBox + "\n\n" + "\nQuantity                : " + numberOfQuantity + "\n\n\n" + "   Total Price  : "
+                + totalPrice + "\n\n";
+
+            }
+
+            else 
+
+            {
+
+                totalPrice = regularCoffeePrice * numberOfQuantity;
+                displayRichTextBox.Text = "\nCustomer Information\n\n" + "\nCustomer Name     : " + customerName + "\n\n"
+                + "\nContact No            : " + contactNo + "\n\n" + "\nCustomer Address  : " + customerAddress + "\n\n"
+                + "\nOrder                    : " + orderBox + "\n\n" + "\nQuantity                : " + numberOfQuantity + "\n\n\n" + "   Total Price  : "
+                + totalPrice + "\n\n";
+
+            }
+
+            /*  else if (orderBox == "Hot")
+
+              {
+                  displayRichTextBox.Text = "\nCustomer Information\n\n\n"; // + "Total Price : " + totalPrice "\n\n";
+                  displayRichTextBox.Text = "\nCustomer Name    : " + customerName + "\n\n";
+                  displayRichTextBox.Text = "\nContact No       : " + contactNo + "\n\n";
+                  displayRichTextBox.Text = "\nCustomer Address : " + customerAddress + "\n\n";
+                  displayRichTextBox.Text = "\nOrder            : " + orderBox + "\n\n";
+                  displayRichTextBox.Text = "\nQuantity         : " + numberOfQuantity + "\n\n";
+                  //totalPrice = hotCoffeePrice * numberOfQuantity;
+
+              }
+
+              else 
+
+              {
+                  displayRichTextBox.Text = "\nCustomer Information\n\n\n"; // + "Total Price : " + totalPrice "\n\n";
+                  displayRichTextBox.Text = "\nCustomer Name    : " + customerName + "\n\n";
+                  displayRichTextBox.Text = "\nContact No       : " + contactNo + "\n\n";
+                  displayRichTextBox.Text = "\nCustomer Address : " + customerAddress + "\n\n";
+                  displayRichTextBox.Text = "\nOrder            : " + orderBox + "\n\n";
+                  displayRichTextBox.Text = "\nQuantity         : " + numberOfQuantity + "\n\n";
+                  //totalPrice = regularCoffeePrice * numberOfQuantity;
+
+              } */
+
+        }
+    }
+}
--- CoffeeShopApp/CoffeeShopApp/Program.cs
+++ CoffeeShopApp/CoffeeShopApp/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace MyWinApp
+namespace CoffeeShopApp
 {
     static class Program
     {
@@ -16,9 +16,7 @@ namespace MyWinApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new HomeForm());
-            Application.Run(new DataTypeForm());
-
+            Application.Run(new CoffeeShop());
         }
     }
 }
