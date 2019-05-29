8f43a703cf563445ca7401df9dda42d0cf2295bc
unknown <coolbappi8888@gmail.com>
2019-05-29 14:48:49 +0600

    added_folder

---
+++ ExamApps/ExamApps/Home.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace ExamApps
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.ReverseButton = new System.Windows.Forms.Button();
+            this.SumButton = new System.Windows.Forms.Button();
+            this.MaximumButton = new System.Windows.Forms.Button();
+            this.MinimumButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // showRichTextBox
+            // 
+            this.showRichTextBox.Location = new System.Drawing.Point(87, 122);
+            this.showRichTextBox.Name = "showRichTextBox";
+            this.showRichTextBox.Size = new System.Drawing.Size(299, 230);
+            this.showRichTextBox.TabIndex = 0;
+            this.showRichTextBox.Text = "";
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(189, 84);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(197, 20);
+            this.numberTextBox.TabIndex = 1;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(406, 131);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(75, 23);
+            this.AddButton.TabIndex = 2;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(406, 182);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 2;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // ReverseButton
+            // 
+            this.ReverseButton.Location = new System.Drawing.Point(406, 240);
+            this.ReverseButton.Name = "ReverseButton";
+            this.ReverseButton.Size = new System.Drawing.Size(75, 23);
+            this.ReverseButton.TabIndex = 2;
+            this.ReverseButton.Text = "Reverse";
+            this.ReverseButton.UseVisualStyleBackColor = true;
+            this.ReverseButton.Click += new System.EventHandler(this.ReverseButton_Click);
+            // 
+            // SumButton
+            // 
+            this.SumButton.Location = new System.Drawing.Point(406, 304);
+            this.SumButton.Name = "SumButton";
+            this.SumButton.Size = new System.Drawing.Size(75, 23);
+            this.SumButton.TabIndex = 2;
+            this.SumButton.Text = "Sum";
+            this.SumButton.UseVisualStyleBackColor = true;
+            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
+            // 
+            // MaximumButton
+            // 
+            this.MaximumButton.Location = new System.Drawing.Point(119, 370);
+            this.MaximumButton.Name = "MaximumButton";
+            this.MaximumButton.Size = new System.Drawing.Size(75, 23);
+            this.MaximumButton.TabIndex = 3;
+            this.MaximumButton.Text = "Miximum";
+            this.MaximumButton.UseVisualStyleBackColor = true;
+            this.MaximumButton.Click += new System.EventHandler(this.MaximumButton_Click);
+            // 
+            // MinimumButton
+            // 
+            this.MinimumButton.Location = new System.Drawing.Point(240, 370);
+            this.MinimumButton.Name = "MinimumButton";
+            this.MinimumButton.Size = new System.Drawing.Size(75, 23);
+            this.MinimumButton.TabIndex = 4;
+            this.MinimumButton.Text = "Minimum";
+            this.MinimumButton.UseVisualStyleBackColor = true;
+            this.MinimumButton.Click += new System.EventHandler(this.MinimumButton_Click);
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MinimumButton);
+            this.Controls.Add(this.MaximumButton);
+            this.Controls.Add(this.SumButton);
+            this.Controls.Add(this.ReverseButton);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.showRichTextBox);
+            this.Name = "Home";
+            this.Text = "Home";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox showRichTextBox;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.Button ReverseButton;
+        private System.Windows.Forms.Button SumButton;
+        private System.Windows.Forms.Button MaximumButton;
+        private System.Windows.Forms.Button MinimumButton;
+    }
+}
+
---
+++ ExamApps/ExamApps/Home.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamApps
+{
+    public partial class Home : Form
+    {
+        const int size = 10;
+        int[] number = new int[size];
+        int[] max = new int[size];
+        int[] min = new int[size];
+        int index = 0;
+        
+
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+
+
+
+            string message = "";
+            number[index] = Convert.ToInt32(numberTextBox.Text);
+            index++;
+            for (int index = 0; index < number.Length; index++)
+            {
+                if (number[index] != 0)
+
+                    message = message +"element " + index + " - " + number[index].ToString() +"\n";
+
+            }
+
+            showRichTextBox.Text = message +"\n";
+            numberTextBox.Clear();
+
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            string message = "";
+            //number[index] = Convert.ToInt32(numberTextBox.Text);
+            index++;
+            for (int index = 0; index < number.Length; index++)
+            {
+                if (number[index] != 0)
+
+                    message = message + "element " + index + " - " + number[index].ToString() + "\n";
+
+            }
+
+            showRichTextBox.Text = message+"\n";
+            numberTextBox.Clear();
+
+        }
+
+        private void ReverseButton_Click(object sender, EventArgs e)
+        {
+          string message = "";
+             for (int index = number.Length-1;index>=0; index--)
+            {
+                if (number[index] != 0)
+
+                    message = message + "element " + index + " - " + number[index].ToString() + "\n";
+
+            }
+
+            showRichTextBox.Text = message;
+            numberTextBox.Clear();
+
+        }
+
+        private void SumButton_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            for (int index = 0; index < number.Length; index++)
+            {
+                if (number[index] != 0)
+
+                    sum = sum + number[index];
+
+            }
+            showRichTextBox.Text = "Sum :" + sum;
+            numberTextBox.Clear();
+        }
+
+        private void MaximumButton_Click(object sender, EventArgs e)
+        {
+            int max = number[0];
+            string message = "";
+            //number[index] = Convert.ToInt32(numberTextBox.Text);
+            index++;
+            for (int index = 0; index < number.Length; index++)
+            {
+                if (number[index] != 0)
+
+                    message = message + "element " + index + " - " + number[index].ToString() + "\n";
+
+            }
+
+            showRichTextBox.Text = message + "\n";
+
+
+
+
+            for (int index = 0; index < number.Length; index++)
+
+            {
+
+                if (number[index] > max)
+                {
+                    max = number[index];
+                }
+
+
+                showRichTextBox.Text = "Maximum element is :" + max + "\n";
+
+
+               }
+            numberTextBox.Clear();
+
+        }
+
+        private void MinimumButton_Click(object sender, EventArgs e)
+        {
+
+            int min = number[0];
+            string message = "";
+            //number[index] = Convert.ToInt32(numberTextBox.Text);
+            index++;
+            for (int index = 0; index < number.Length; index++)
+            {
+                if (number[index] != 0)
+
+                    message = message + "element " + index + " - " + number[index].ToString() + "\n";
+
+            }
+
+            showRichTextBox.Text = message + "\n";
+
+
+
+
+            for (int index = 0; index < number[index]; index++)
+
+            {
+
+                if (number[index] < min)
+                {
+                    min = number[index];
+                }
+
+                showRichTextBox.Text = "Minimum element is :" + min + "\n";
+
+
+            }
+            numberTextBox.Clear();
+        }
+    }
+}
