c5c5254abaca073ba6826a414aeecce4dc217392
unknown <coolbappi8888@gmail.com>
2019-06-16 23:27:58 +0600

    added_Folder

---
+++ AdoApps/AdoApps/AdoApps.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace AdoApps
+{
+    partial class AdoApps
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(223, 199);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 0;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // AdoApps
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 411);
+            this.Controls.Add(this.SaveButton);
+            this.Name = "AdoApps";
+            this.Text = "DepartmentsUi";
+            this.Load += new System.EventHandler(this.AdoApps_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ AdoApps/AdoApps/AdoApps.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoApps
+{
+    public partial class AdoApps : Form
+    {
+        public AdoApps()
+        {
+            InitializeComponent();
+        }
+
+        private void AdoApps_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //
+             
+                SqlConnection sqlConnection = new SqlConnection();
+                String connectionString = @"server=DESKTOP-GPO65HT;Database= ADODB;integrated Security=True";
+                sqlConnection.ConnectionString = connectionString;
+
+              //
+                SqlCommand sqlCommand = new SqlCommand();
+                string commandString = @"INSERT INTO Departments Values ('Computer Science and Engineering','CSE')";
+                sqlCommand.CommandText = commandString;
+                sqlCommand.Connection = sqlConnection;
+
+                //
+                sqlConnection.Open();
+
+                sqlCommand.ExecuteNonQuery();
+
+
+                //
+                sqlConnection.Close();
+
+
+
+            }
+
+            catch (Exception exception)
+            {
+
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+    }
+}
