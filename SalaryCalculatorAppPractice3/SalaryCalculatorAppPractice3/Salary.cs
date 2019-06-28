5a5a0a860ef7f2697cfd809c953ab21e1f5ab66c
unknown <coolbappi8888@gmail.com>
2019-06-28 19:29:33 +0600

    added_folder

---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorAppPractice3/Salary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorAppPractice3
+{
+    class Salary
+    {
+        public string employeeName;
+        public int basicAmount;
+        public int homeRent;
+        public int medicalAllowance;
+
+
+        public int GetHomeRent()
+        {
+
+            int homeRent = basicAmount / 100;
+            return homeRent;
+
+        }
+
+
+        public int GetMedicalAllowance()
+        {
+
+            int medicalAllowance = basicAmount /100;
+            return medicalAllowance;
+
+        }
+
+
+
+        public int GetSalary()
+        {
+
+            int TotalSalary = basicAmount + homeRent + medicalAllowance;
+            return TotalSalary;
+
+        }
+
+
+    }
+}
---
+++ SalaryCalculatorAppPractice3/SalaryCalculatorAppPractice3/SalaryCalculatorUi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorAppPractice3
+{
+    public partial class SalaryCalculatorUi : Form
+    {
+        public SalaryCalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowMeSalaryButton_Click(object sender, EventArgs e)
+        {
+            Salary salaryobj = new Salary();
+            salaryobj.employeeName = employeeNameTextBox.Text;
+            salaryobj.basicAmount =Convert.ToInt32( basicAmountTextBox.Text);
+            salaryobj.homeRent = Convert.ToInt32(homeRentTextBox.Text);
+            salaryobj.medicalAllowance = Convert.ToInt32(medicalAllowanceTextBox.Text);
+
+        
+           string Show=salaryobj.GetSalary().ToString();
+            MessageBox.Show(Show);
+
+        }
+    }
+}
