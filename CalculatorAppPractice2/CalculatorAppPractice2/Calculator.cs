e918916b055c308361b413726d0b30d7ce2592ca
unknown <coolbappi8888@gmail.com>
2019-06-28 17:07:27 +0600

    added_folder

---
+++ CalculatorAppPractice2/CalculatorAppPractice2/Calculator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorAppPractice2
+{
+    public class Calculator
+    {
+
+        public int firstNumber;
+        public int secondNumber;
+        public int result;
+
+
+        public int GetAdd()
+        {
+
+            result = firstNumber + secondNumber;
+            return result;
+
+        }
+
+
+        public int GetSubtract()
+        {
+
+            result = firstNumber - secondNumber;
+            return result;
+
+        }
+
+
+        public int GetMultiply()
+        {
+
+            result = firstNumber * secondNumber;
+            return result;
+
+        }
+
+
+        public int GetDivide()
+        {
+
+            result = firstNumber / secondNumber;
+            return result;
+
+        }
+
+
+
+
+    }
+}
---
+++ CalculatorAppPractice2/CalculatorAppPractice2/CalculatorUi.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorAppPractice2
+{
+    public partial class CalculatorUi : Form
+    {
+
+        public CalculatorUi()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            Calculator calculatorobj = new Calculator();
+            calculatorobj.firstNumber = Convert.ToInt32(firstNumberTextBox.Text);
+            calculatorobj.secondNumber = Convert.ToInt32(secondNumberTextBox.Text);
+   
+            resultTextBox.Text = calculatorobj.GetAdd().ToString();
+
+        }
+
+        private void SubtractButton_Click(object sender, EventArgs e)
+        {
+
+            Calculator calculatorobj = new Calculator();
+            calculatorobj.firstNumber = Convert.ToInt32(firstNumberTextBox.Text);
+            calculatorobj.secondNumber = Convert.ToInt32(secondNumberTextBox.Text);
+
+            resultTextBox.Text = calculatorobj.GetSubtract().ToString();
+        }
+
+        private void MultiplyButton_Click(object sender, EventArgs e)
+        {
+            Calculator calculatorobj = new Calculator();
+            calculatorobj.firstNumber = Convert.ToInt32(firstNumberTextBox.Text);
+            calculatorobj.secondNumber = Convert.ToInt32(secondNumberTextBox.Text);
+
+            resultTextBox.Text = calculatorobj.GetMultiply().ToString();
+        }
+
+        private void DivideButton_Click(object sender, EventArgs e)
+        {
+            Calculator calculatorobj = new Calculator();
+            calculatorobj.firstNumber = Convert.ToInt32(firstNumberTextBox.Text);
+            calculatorobj.secondNumber = Convert.ToInt32(secondNumberTextBox.Text);
+
+            resultTextBox.Text = calculatorobj.GetDivide().ToString();
+        }
+    }
+}
