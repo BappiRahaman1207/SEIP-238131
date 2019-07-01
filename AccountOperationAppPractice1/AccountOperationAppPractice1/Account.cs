4798d9a2e25609bbf47760d857ff34951b36502a
unknown <coolbappi8888@gmail.com>
2019-07-01 20:22:16 +0600

    added_folder

---
+++ AccountOperationAppPractice1/AccountOperationAppPractice1/Account.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountOperationAppPractice1
+{
+    class Account
+    {
+
+        public int accountNumber;
+        public string customerName;
+        public int amount;
+        public int diposit;
+        public int withdraw;
+
+
+
+
+        public int GetAmount()
+        {
+            int amount = 0;
+            return amount;
+
+        }
+
+        public int GetDiposit()
+        {
+
+       
+            diposit = amount + diposit;
+            return diposit;
+
+        }
+
+        public int GetWithdraw()
+        {
+            withdraw = amount - withdraw;
+            return withdraw;
+
+        }
+
+     
+
+
+    }
+}
---
+++ AccountOperationAppPractice1/AccountOperationAppPractice1/AccountOperationUi.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationAppPractice1
+{
+    public partial class AccountOperationUi : Form
+    {
+        public AccountOperationUi()
+        {
+            InitializeComponent();
+        }
+        Account accountObj = new Account();
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        { 
+
+            accountObj.accountNumber = Convert.ToInt32(accountNoTextBox.Text);
+            accountObj.customerName = customerNameTextBox.Text;
+            
+        }
+
+        private void DipositButton_Click(object sender, EventArgs e)
+        {
+            accountObj.GetDiposit();
+            accountObj.amount = Convert.ToInt32(amountTextBox.Text);
+          
+        }
+
+        private void WithdrawButton_Click(object sender, EventArgs e)
+        {
+            accountObj.GetWithdraw();
+            accountObj.amount = Convert.ToInt32(amountTextBox.Text);
+          
+        }
+
+        private void ReportButton_Click(object sender, EventArgs e)
+        {
+            int accountNumber = accountObj.accountNumber;
+            string customerName = accountObj.customerName;
+            int amount = accountObj.amount;
+            MessageBox.Show("Account Number:"+ accountNumber+"\t"+"Customer Name:"+ customerName+"\t"+"Blance:"+ amount+" Taka");
+        }
+    }
+}
